ed355a39632938415a02928d57868ba42c08c9c2
EmilioOldenziel <emilio.oldenziel@gmail.com>
2018-04-29 20:10:26 +0200

    readme and comments update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SubtitlesNLP
-Subtitle NLP
+
+Processing subtitles in XML format  
+make sure you use Python 3.6  
+
+To word count the subtitles using the Python 3.6 standard lib  
+```
+python word_count_native.py  
+```  
+
+To word count the subtitles using Apache Spark open ```notebooks/spark_word_count.ipynb ``` make sure you have pyspark installed
--- word_count_native.py
+++ word_count_native.py
@@ -3,14 +3,20 @@ from gzip import GzipFile
 import xml.etree.ElementTree as etree
 from pprint import pprint
 
-
+# read all filenames from the subtitles directory (only Frisian for now)
 file_names = glob.glob("subtitles/**/*.xml.gz", recursive=True)
+
+# wordcount dictionary
 words_dict = {}
 
 for file_name in file_names:
+    # unzip stream
     f = GzipFile(file_name)
+
+    # iterative parse xml per element
     xml_iter = iter(etree.iterparse(f, events=('start', 'end')))
 
+    # get and count word
     for event, element in xml_iter:
         if event == 'start' and element.tag == 'w':
             if element.text in words_dict:
@@ -20,6 +26,7 @@ for file_name in file_names:
 
 print(f"{len(words_dict)} words")
 
-with open('fr.json', 'w') as outfile:
+# write wordcount dictionary to json file
+with open('words.json', 'w') as outfile:
     json.dump(words_dict, outfile)
 
