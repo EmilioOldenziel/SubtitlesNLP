b8bb9416aca3a4b35e4c943c0c2861a6b8bc284a
EmilioOldenziel <emilio.oldenziel@gmail.com>
2018-05-02 12:17:11 +0200

    spark files completed

--- spark_word_count_sorted.py
+++ spark_word_count_sorted.py
@@ -1,8 +1,13 @@
-
 # coding: utf-8
+
+import argparse
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode, count
 from os import path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--l', help='give language', required=True)
+args = parser.parse_args()
 
 # warehouse_location points to the default location for managed databases and tables
 warehouse_location = path.abspath('spark-warehouse')
@@ -11,10 +16,12 @@ spark = SparkSession \
     .builder \
     .appName("Python Spark wordcount") \
     .config("spark.sql.warehouse.dir", warehouse_location) \
-    .enableHiveSupport()
+    .enableHiveSupport() \
     .getOrCreate()
 
-df_subtitles = spark.sql(f"SELECT * FROM nl")
+language = args.l
+
+df_subtitles = spark.sql("SELECT * FROM " + language)
 
 df_wc = df_subtitles \
     .withColumn("word", explode(col("w"))) \
@@ -23,9 +30,9 @@ df_wc = df_subtitles \
     .select(col("word"),col("count(word)").alias("frequency")) \
     .sort(col("frequency").desc())
 
-print(df_wc.count(), "words found")
+print(df_wc.count(), "words counted for", language)
 print("saving dataframe...")
-df_wc.write.mode("overwrite").saveAsTable("wc_nl")
-print("saved, language")
+df_wc.write.mode("overwrite").saveAsTable("wc_"+language)
+print("saved", language)
 
 spark.sparkContext.stop()
--- spark_xml_to_dataframe.py
+++ spark_xml_to_dataframe.py
@@ -1,13 +1,22 @@
 
 # coding: utf-8
+
+import argparse
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 from os import environ, path
 
+# get language to read argument
+parser = argparse.ArgumentParser()
+parser.add_argument('--l', help='give language', required=True)
+args = parser.parse_args()
+
 # warehouse_location points to the default location for managed databases and tables
 warehouse_location = path.abspath('spark-warehouse')
 
+# load spark-xml
 environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.databricks:spark-xml_2.11:0.4.1 pyspark-shell' 
+
 spark = SparkSession \
     .builder \
     .appName("Python Spark wordcount") \
@@ -18,13 +27,16 @@ schema = StructType([
     StructField('w', ArrayType(StringType()))
 ])
 
+language = args.l
+
 # read xml files as df
 df_xml = spark.read \
     .format("com.databricks.spark.xml") \
     .option("rowTag", "s") \
     .option("rootTag", "document") \
-    .load("./subtitles/lt_all/*.xml", schema=schema) # spark does not support recursive load
+    .load("./subtitles/"+ language + "/*/*/*.xml.gz", schema=schema) # spark does not support recursive load
 
-df_xml.write.mode("overwrite").saveAsTable("nl")
+print("saving", language)
+df_xml.write.mode("overwrite").saveAsTable(language)
 
 spark.sparkContext.stop()
