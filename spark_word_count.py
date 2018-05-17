2972f97f124693dd255b256af15b93582684ec2d
EmilioOldenziel <emilio.oldenziel@gmail.com>
2018-05-18 01:39:45 +0200

    spark revectoring

--- spark_word_count.py
+++ spark_word_count.py
@@ -1,34 +1,20 @@
 # coding: utf-8
 
 import argparse
-from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode, count, lower
-from os import path, environ
+from spark import Spark
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--language', help='give language to parse', required=True)
 args = parser.parse_args()
 
-# warehouse_location points to the default location for managed databases and tables
-warehouse_location = path.abspath('spark-warehouse')
-
-# set driver memory
-submit_args = [
-    '--driver-memory 15g',
-    'pyspark-shell'
-]
-environ['PYSPARK_SUBMIT_ARGS'] =  " ".join(submit_args)
-
-spark = SparkSession \
-    .builder \
-    .appName("Python Spark wordcount") \
-    .config("spark.sql.warehouse.dir", warehouse_location) \
-    .enableHiveSupport() \
-    .getOrCreate()
+# create spark session
+sp = Spark()
+spark_session = sp.make_spark_session()
 
 language = args.language
 
-df_subtitles = spark.sql("SELECT * FROM " + language)
+df_subtitles = sp.load_table(spark_session, language)
 
 df_wc = df_subtitles \
     .withColumn("word", explode(col("w"))) \
@@ -37,8 +23,6 @@ df_wc = df_subtitles \
     .groupBy("word").agg(count(col("word"))) \
     .select(col("word"),col("count(word)").alias("frequency")) \
 
-print("saving dataframe...")
-df_wc.write.mode("overwrite").saveAsTable("wc_"+language)
-print("saved", language)
+sp.save_table(df_wc, "wc_"+language)
 
-spark.sparkContext.stop()
+sp.stop(spark_session)
